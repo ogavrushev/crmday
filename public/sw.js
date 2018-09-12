b512e833567637a7fe9d8878e00f3066acfdd9f7
Oleg Gavrushev <oleg.gavrushev@yandex.ru>
2018-09-12 22:59:22 +0300

    update styles for 2k screens

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 //This is the service worker with the Cache-first network
 
-var cacheName = 'sipuni-crmday-nizhni-v3';
+var cacheName = 'sipuni-crmday-nizhni-v4';
 var files = [
     'https://fonts.googleapis.com/css?family=PT+Sans',
     'img/icons/apple-touch-icon.png',
